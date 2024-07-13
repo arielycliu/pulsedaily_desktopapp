a45e3854f70c4a94a16d0552bf332bc5938a4395
arieycliu <ariel.yc.liu@gmail.com>
2024-07-13 13:31:09 -0400

    Update build_msi path

--- pulsedaily/build_msi.js
+++ pulsedaily/build_msi.js
@@ -1,7 +1,7 @@
 const { MSICreator } = require('electron-wix-msi');
 
-const APP_DIR = '/Users/ariel/Documents/pulsedailydemo/Electron2/pulsedaily/dist/win-unpacked/pulsedaily.exe';
-const OUT_DIR = '/Users/ariel/Documents/pulsedailydemo/Electron2/pulsedaily/out/msi/pulsedaily';
+const APP_DIR = '/Users/ariel/Documents/pulsedaily_desktopapp/pulsedaily/dist/win-unpacked/pulsedaily.exe';
+const OUT_DIR = '/Users/ariel/Documents/pulsedaily_desktopapp/pulsedaily/out/msi/pulsedaily';
 
 // Step 1: Instantiate the MSICreator
 const msiCreator = new MSICreator({
